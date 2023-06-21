3e1ea390635be714df0af8b4f82b2b4701c5b642
Stanislaw Bidowaniec <20791742+s-bidowaniec@users.noreply.github.com>
2023-06-21 23:06:53 +0200

    fix docker production image

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias['@/lib'] = path.join(__dirname, 'lib');
+    return config;
+  }
 }
 
 export default nextConfig
